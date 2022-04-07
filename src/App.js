460294b9a13e2624e91334dd0a369dfd3bbe58e4
Cânciu Ionuț - Cristian <icanci01@ucy.ac.cy>
2022-04-08 00:48:48 +0300

    login-form + filestructure #[must implement the ftp login]

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import LoginForm from "./components/LoginForm";
 
 function App() {
+  const adminUser = {
+    username: "admin",
+    password: "admin",
+  };
+
+  const [user, setUser] = useState({ username: "", password: "" });
+  const [error, setError] = useState("");
+
+  const Login = (details) => {
+    console.log(details);
+
+    if (
+      details.username == adminUser.username &&
+      details.password == adminUser.password
+    ) {
+      console.log("Login Successful");
+      setUser({
+        username: details.username,
+        password: details.password,
+      });
+    } else {
+      console.log("Login Failed");
+      setError("Login Failed");
+    }
+  };
+
+  const Logout = () => {
+    console.log("logout");
+    setUser({
+      username: "",
+      password: "",
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="body">
+        {/* If the user is logged in print initial dashboard */}
+        {user.username != "" ? (
+          <div className="welcome">
+            <Header />
+            <h2>
+              Welcome <span>{user.username}</span>
+            </h2>
+            <button onClick={Logout}>Logout</button>
+            <Footer />
+          </div>
+        ) : (
+          <LoginForm Login={Login} error={error} />
+        )}
+      </div>
     </div>
   );
 }
 
 export default App;
+
